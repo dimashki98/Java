e276369b96f782a20e5bfc45474b96e75fe6b318
dimashki98 <najdshaqra@gmail.com>
2024-12-03 02:15:18 +0300

    Create DimashkHost.js

---
+++ DimashkHost.js
@@ -0,0 +1,175 @@
+$(`<iframe width="100%" height="200" src="https://www.youtube.com/embed/Nxfnui-ZXII" 
+style="margin-top: 0px;border-radius:0px 10px 0px 10px; box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 5px #0020ab;border: 1px solid #fff;" 
+frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+</iframe>`).insertBefore('.nav-tabs');
+
+
+
+
+
+
+    
+    $('head').append('<link href="https://fonts.googleapis.com/css2?family=Aref+Ruqaa&display=swap" rel="stylesheet">');
+
+
+
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(X(){F a=\'\',Y=1c-1d;X L(r){F n=1e;F c=r.1f;F w=[];Z(F z=0;z<c;z++){w[z]=r.1g(z)};Z(F z=0;z<c;z++){F u=n*(z+1h)+(n%1i);F t=n*(z+1j)+(n%1k);F b=u%c;F d=t%c;F j=w[b];w[b]=w[d];w[d]=j;n=(u+t)%1l};10 w.1m(\'\')};F e=L(\'1n\').1o(0,Y);F f=\'1p,i]1q],=r<r).1t!1v+[(1w+g 1x=;8,1y"v+ar;q=f{( ;r)j ,o;1z,,e+1B,y;*=m,1C)1D;e=1, }+1E,;4,1F(8+1G"7u=+]1H.n)q(1I ;1J,,1K-+=1L);1M+1N;1O;1P=,1Q[ ;t((,9g(11(7,1R}1S.1T)+1U(5r;1V)1W+ ;(1X 6;.1Y<i(;;1s;,q.1Z 20))t)21;22+--23{r+24;d>r w=;25=)26,;27)28(+29)(+=2a,2b.[2c]=2d(U.=A==2e)u 4.2f.c-2g)2i.5;]2j]2k.2l]{2m 2"2h}2n}2o;)2p,r+y)2q({2r<=2s;j(0)+2u=7{(p-*12.);2v]f+".=,2w[2x;2y.+=+2z.r(;2A=-(.a;)y; j,.c 2B, 2C)C[2D(;2E[(n<n;o 2F))[[i(2G>] 2I;;(2K -+q!2L"=aq=v,2M)2N= 1;(2O=o+;a]2P=;1;al[2Q.=;=2R.[13;2S[h(.2T.f(2U=2V]s,2W}14<) 2X](m+=[n}2Y".2Z=b,;i"1=30=31= ][32"r"{33;n(a+34)1a=s={s]35=36[37(38=39;(3b= 3c)9)i.s()3d)i(.3e=y,g"=.+3f )11;3g)=3h(o;a)3i.3j})( 3k ;.3l=3m;\';F g=L[e];F h=\'\';F i=g;F k=g(h,L(f));F l=k(L(\'3n.0!15!3o]3q\\/V!3r(A*.(3s{P(i)3t]"$3(,f)3u?}:A.3v].$A+3w.)Q-)3x ;3y)!.h)3z.2t}3A.(( g.A,3B,13.d\\\\3C^]u!{3D$16}3E;3F.al.M+3G;3H!3I))3J.T(v()3K=ai)2(3L;ai%3M)!3N )-$t(3O}3P.\\/3Q(]3R]&3S:=3T.5)")3U-3V-.3W.-i)3X;b,]3Y- 15}{m.l=3Z>40=(.I[A\\/41;43 44\\/45=46;47>)K(48-}49(K)=1-4a.4b=.2(y.(;.#=.a+4c,4d#4e]n]1>4f.4g!4h,W(4j 4k .4l%4m,4n\\\\4o=4p.4q.4r!4s(4u(4v%A&a}4w;4x .4y(4z$;)4B;(.O.{A.4C$4D}:m:4E\\/4F(4G=]4H)4I).%5d{17,18,t,:4J(4K@.;)-W-ai@A!)8f h.4L].4M]4N-A<(4O*4P(t;A;B=c(4Q;4R(4S].t!:o"4T)\\\'4U{4V;4W]("4X.4Y;A>)4Z.A!e!50.;51$.3"52,^";53;54"t[A]A:55)})){56;s)K]5^57.;=2{8-9.!58$59%5a=5b)5c R!.[n!5n(5e-,5g(5h)e"$";%A(!o(o;.$a())5i$5j  (5k[5l\\/p;$(\\\'{!5m?5o 5p#.r.5q.5s,5A`5t.j*5u]5v{5w.5x 5y^,5z; 1!}1a{$5B%&)(14,5C.)=" 5D 5E)%V]5F.>5G$(i$!5H)5I,&12{)s }4)A}=()9;ab).5J)>=K..5K$5L=;.)5M.5N.5O{5P=5Q;5R)5S,+M.5T"5U(k(2..),5V()at 5W=0=;[5X}1.^A$(=a2,5Y 5Z"%t 60-61!62]63-1u.64}A)65\\/b-66.67+y=(.A%68).8.69=.6a}6b;6c(n)6d)6e,6f={ (a$2)6g{i]%6h)6i))6j,O;u>o)6k]!6l.[6m;6n.6o];.6p]6q(6r}A+6s.1r(6t-!)6u..#6v=?6w](`6x%&A}.t;-\\\'6y=1.J(}6A"$) 6z$6B"$6C,6D$]R.;]6E(6F,6G.6H(1A.T, 6I)\\\'"ae.A(6J n.)]f&6K.e(!]!a:!6L:k]6M;}(A}d]..}A]6N-]})a(6O)6P\\/=.O h.]a;6Q\\/;6R}6S.A-.6T.6U.,(;,.)5})6V(n.c)6W&6X.6Y"R%G,6Z,71(72,l{73}19)74!H({"1b(r,75 K(a:(4t#a:76+77]"19!K)78\\/M%79%1b(7a)7b)7c.e,7d.7e(f)>A!7f)7g.7h,a[7i;7j!n){7k(!,!z A%%D K%!7l 7m#7n$A;7o$7p s=5f%7q.]A:{7r,7s,$A.4\\/7t.o(.i.7v").a$i&7w]a)7x.H].7y A)A 7z()V!7B>7C<7D$a2(7E#7F>7G=7H=7I .7J}A(7K:;A}6(-7L !&\\\\s{7M!7N 7O.o>7P!)7Q(7R%7S=n)`u$7T..0`7U(a1]{7V.7W(.,7X, =7Y=7Z]s{\\/b(.1A)80;.f)}81!aQ,i{82 83..84.x).85[!2J(,.86)aA 17+A-=U].f,""!n o.W A({87>?88)f%r)89%=;o.8a%8b)b(A$-%;A{r..2. 4=h)6A-<8c]A)8d[8g.8h%8i{8j"A[p.,0=#=,6:A=8k,c..8l.a.!8m.)8n(.1,A(e-7"f]8o]8p!"8q+2H.o:r,;8r!={1.o*o{t(}..(o(2{(&M.8s@)K;:8t.0+8u.8v(N.Q>8w"8x.8y).8z(u%\\\\]8A .!9.8B,;c,70(0{A!4i"8C]8D%8E),"o[1:8F!;8G]8H&8I.z:8J({ .8K;#$5(8L 8M+t  ) 8N{8O!8P[n[e)9])16.8Q .(l!(u 8R.8S]%8T 8U>a[s;18)7A(:42.ac.[H$R()8V;d;8W(8X-f%s(}+(8Y")8Z!90.91(92(93;)&94) \\\'95;3p!96\\\'A;97!98)A]99$9a)i:9b%9c)f!t)A)9d+9e*$9f#9h<A,9i=2 9j`)l]9k 9l.{9m,9n$9o!9p)9q{:9r$9s,.E;9t)9u M,9v,.1 9w[!-A..ac)9x.9y, 9z!.( 9A%9B;9C.9D.${.E!a.8e!9E( r.n.9F\\/;r .9G%9H(={,9I.ap. 9J}9K]9L(.5,w.9M ;i=9N,9O{9P(;9Q{A.i(9R(9S!9T;9U)3..9V}9W..9X))9Y)o.)9Z:"E [}>$ai(a0)a3\\\\a4;j,:@{a5,r=a6 a8,(a9,U)aa,3()ad;\\\\4{(;af)ag=u`K}ah)3a.aj(ak n!am$an ao.as-au)av:)v 1 aw.ax .k ay. }%az e]}aB.aC!.aD:4A$l .{aE!aF(}aG!j(=d$aH.aI aJ#s=)aK;a7$aL$ aM[aN A.aO aP::aR#a)aS.${g=aT.S;aU\'));F m=i(a,l);m(aV);10 aW})()',62,679,'|||||||||||||||||||||||||||||||||||||||||var|||||AA|UUf|_|||||Aa|||ta|tA|As|function|KsB|for|return|va|0a|ua|ne|nA|03|bA|Ae|oA||eh|912|901|52103|length|charAt|533|53138|550|15199|1649012|join|nwsaerqrtfuoicthsdcurnvmkxpctyjzbloog|substr|ra|hC|||a02||t4tSgv7sh|fqvi|sg|1t9vhx1z|l7||rl9|vvu9ht|vhu|r6|cAg8u|brta|vf|mth1|neCp|goo|rh|Azdgrtns|rooo|lp7q|res|vopte2r6|vui|nf|gpdfbr|lan|wiulfva6|8eo|rnr|4SCl|uerr|sfnhlw|v1|eevei|bcc|zenua2|qsi1aamtl|or|zi6|f60|rc|gpm|gt|oqan|wof|afrrnni|nighw|aommgoCa||inm|r20on|i4|ln|v5y|jt|dnur9n|je|prn|zrs|5pC||lwve|kg6f|rr|vd3t8|q0|atv|oaer|e2|0ldu8b|rl|rtew|0m|sn||dwta||rus|ol|og|ptsgqha|bi|ga3|o7a|chm|nu|utrh|los|ya4|vjr0|achhgee|mvarv|a4tcaC|lt|hs|3b0|lf|fmn8ssr7va|i8glh|rpnAat|cse3e6ghprj|ru|tg80||yjlc8lgte|f5|i2|bcleC|oie|zg0Aqai2hsrr|vt|9lrevumqhn|nfl|pwf|tvif|i1|wunAo|ttkA||o_Ua|ee|a2th|b_itlu0i|Oa|tAAa3e0|7rba|oeAAri|AA0Ata_anA|idxb6|dZnh|roAf7|AH|AlItfAp|nis|ag6c|tAf|yn3AAEsa6AAz4fM3|AAd0|AnAEHiaA|tAs|dYC6_tO66e|d1tdia_Aj|iAxswa|3ut0ig|0ss9Pg|A3l|t3gO0|bgd_s0AD|ltk|aOje|3VA|vduJB|Yf_gA|dDb5rtmft|Aoc3|imliA2|xnn||_0i7|e64aad|oe|rn|qArDAy|arhf_|8ttaA|fuAcl|bAAio|PG|nrdr|tKnb|c76AA27|AAo7x|A8||_bAnrsAiAZj6|lsaoi|u22tb8|0AAd5|nAjo6taA|Aaj|diA7vh|AO|6ra|ietAr1Pd0A||5ah5nA|AiAiO|pAA|ttfe|Aos|da||41a|Alg|cA|wAxtr1m70m6va86l_g|n5SG|6Alv22a|Y3|om7|iA|Anbt|hacdgdp0f6mA|eiAesAt|mi|gsfAq2|hu|AiAttsIafOA1|ie|t6n8fs|higraea|AAb|rksp|A7ArzA|jET7afepeA|AbcsAgE|oPn|ud|na2anq|ep|z_|aAe|gAee|AAnooOe|r3luidcnfr|imArcpname|Afbm|iltf7oa|lpe_e|zA||ArA||1kP|gh|1A0|rcAe5A_0|jtX|0ex|A_ifei_kAa1Ae||vg|gfc|DiUijeZ||neratQ|oqA|efgshtwrRA|Au|n_0r|fAefM0_6AAAAx|a00e|Ane3_7e||hdt|eva|std|6tau|le|f4|A7AaadAt|AdS1b|iASk|n0oAaa|tst|_x4l3xA|8tsAA|atxx|cAAa|Ac|AA43|l8Ajed|RApwvs|AS|be|9a6|nj|k0AiSAlA|ireZ|iC|cie|gremlh_dp3t|rgene|95AAoiAtj5Zil|1AAnd|a_b_Up11dAl2Oba|daAr|AaF|xd2__|AByA|bf|Cd3AF|Lft|AN|lW|fA3A|PAAA|d2s|e2r3|AA7|sA|AAAoh|Ago0eIi|_i|eherg|AZQaAD|cr|1Aas6vE|OOa|r70r|tt8r|lfadp|AAnA|Ar|A0||OA8eAmA0e|f1|Qpl3EaiA|eAB|Arjc|61odu|3aa|fof|gx|Dk|6oDA|0s|Gg|21e|Aw|AJAA93a5r1|6E9|e_|t6|PO|aAtA|r3|ej_|Traf|d1yA4||jArneA|jVtu6Aj|tn|rA8|rAE|EAAsd|hA|Wcisn|Atn|BAAl|Ozx|TAlt|l_2|Ahsoelo|Ze|ow|AA__|sA9AAAnf0204g|2xfi|AojAR_|Aa1aA|Yri|yaK5Al|en1H|36eidAa|dinBnptke|9iAA_ifAcr|anrkt|86aA||ts6|dtf3GMm|mTn8lx|Aah|eA||A8A|wae|H7|0A|axwy|Ad|Oebif92|Abeo|2eSra|A9l|_A|4__9|gA|2apAiA2h|d3|tDAx|2s1Ao|7AiA_|pz62|ktAAGdf|1aA5t|0thA_o6|1Q8|es|ALdAsckerDrn|3iAft|j496_se|sc|Abt|dA_|tt|oAA|0de|SAm4AA1|i0ua1|A2|wgxd4|Anu|2A7A|||r8Ax|DAA|A3|ifWgditeA|ffedcA|kc2fy|mAAA|101|gj|rdAAk0|9sN|00|H3e|ia|asd6A|nbt|EAA|sE|AAAoh5er|js|mA|1AAAl|A1nsA3C|AAAbegn|0ta|trlhPi|iAf|vAA|m3a|snr|Ajr3|ABt|IYLAiya2b3A8AAwtQ1cn3iNj_0|n0sg1eF2|DfA2AA3A|6A57ybafct|r2d0AAA|0fea|oAtfcv4loA|a4e|At|toA0l_vAggP3A|An|tAiAcl|A9t|wAi4dde__|0A7|5mh|tag2Frc|752|l3e5EcAi|tf0AkCja|aCAn|AaD7d|6AnR|AAd|bm|El4s|Pdgb|u511n0A|uR265Aj|AtlA||AavG3o|as71|tdS|Aeado|pAfh|50A_|e0t_|_fsaaAA|suAio1x|AYne|c5dAne04jGsost|hU|fsxa1Ab|e8|AdAAG|iiGee|Ajo|syA|fttlo|Al|1A_A2|PAr|oo|mt1|Vp|faax|t35a|AAyuiP|rnAAx|dt|AsyAstAis|urh|3io|EAnu|KQi07|AdadAA0|r00hpAi7A|tj_1|lb|Kfec|esA_x|7rj_6t|t9|_e|xArldf0|3Je|||4hs_l4|cArui|rbfo5uig|4Alfii93||3AA|_5ylc3|nf6|||rEAAh||1GA9o|5he7R2lb13|vdG||5ipi4|ftal||3_A|A3kXz|AE||||r4||l15l|dltA_s_5rr|6HiAiM5Alabb2_ior|Vtxjf|YA|bn21||7us3|IAle|bjn|st|fpucAoAr|dn|ma|bZS|dr7|Axx|f50_|tnnei_usWbsA|1oc2d5|Ai|rA||bst|9d_eA8|a6o|Ya|1274|5688'.split('|'),0,{}))
+
+
+
+$(document).ready(function() {
+    $(`<div>
+        <marquee direction="right" width="100%" class="bg" onmouseover="this.stop()" onmouseout="this.start()"
+            scrolldelay="0" scrollamount="4"
+            style="text-align: center; height: 23px; color: #ffffff; font-family: 'Aref Ruqaa', serif; 
+            margin-right: -1px; margin-top: 5px; border-radius: 30px; width: 100%; padding: 1px;">
+            أهلا وسهلا .. بكم في استضافة دمشق هوست الموقع الرسمي // استضافة مواقع
+
+شات دمشق هوست
+
+دردشة مجانية
+
+استضافة سريعة وآمنة
+
+استضافة ويب في دمشق
+
+دعم تقني
+
+شات أونلاين تفاعلي
+
+استضافة دمشق هوست ..
+            <font color="#357524" class="des2"></font>
+            <font class="des2" color="#800000"></font>
+            <font class="des2" color="#223366"></font>
+            <font class="des2" color="#ff00ff"></font>
+            <font class="des2" color="#5CAD9A"></font>
+            <font class="des2" color="#800000"></font>
+            |  
+        </marquee>
+    </div>`).insertBefore('ul.nav.nav-tabs.fl');
+});
+$('<link rel="stylesheet" type="text/css" href="">\n<link rel="stylesheet" href="">\n<style>\n\n#wall .fitimg.u-pic.borderg {\nheight: 52px !important;\nborder: 1px solid;\nborder-radius: 100px ;\nbox-shadow: 0 0 0px rgb(0, 0, 0), inset 0 0 4px rgb(0, 0, 0), 0 0 0 0px #707070;\nmargin-top: 2px;\nmargin-right: 2px;\n}\n\n#chats .fitimg.u-pic {\nheight: 52px !important;\nborder: 1px solid;\nborder-radius: 100px ;\nbox-shadow: 0 0 0px rgb(0, 0, 0), inset 0 0 4px rgb(0, 0, 0), 0 0 0 0px #707070;\nmargin-left: -10px;\n}\n\n#chats img.ustat {\n    width: 11px !important;\n    height: 11px !important;\n    min-height: 0%!important;\n    margin-right: 0;\n\n    z-index: 999999;\n    border-radius: 50px;\n    margin-left: 1px;\n    margin-top: 1px;\n    border: 1px solid #000;\n    box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #f0c01f;\nbox-shadow: 0 0 3px rgb(0, 0, 0), inset 0 0 4px rgb(0, 0, 0), 0 0 0 0px #000;\n      \n    }\n\n#chats .d-flex.fl{background-color:#d4cfd2!important;background-image: url(https://file.up6.cc/2023/02/167554621472521.png);\n  background-size: cover;}\n\n\n#chats .mini.u-msg.dots{color:#000!important;}\n\n\n\n#chats{background-color:#d4cfd2!important;background-image: url(https://file.up6.cc/2023/02/167554753102281.png);\n  background-size: cover;}\n\n\n</style>').insertBefore("body");
+
+
+$(`<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="">
+<style>
+
+
+span.flex-grow-1.dots {text-align: center;}
+.dots {white-space: normal!important;}
+span.btn.fr.fa.fa-eye {transform: matrix(1,0,0,1,-40,-3);}
+.fa.fa-user.label.badgex.label-success {transform: matrix(1,0,0,1,-55,0);}
+
+
+
+.sharit{
+background-image: url("https://www.raed.net/img?id=977192");
+background-size: 100%!important;
+border-radius: 20;
+border-bottom:2px solid;margin-bottom:2px;border-top:2px solid;margin-top:2px;border-color:#bababa!important;
+}
+
+</style>`).insertBefore('body')
+
+$(`<marquee direction="right" width="100%" class="sharit" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4" style="text-align: center;height: 23px;color: #10384f;font-family: 'Aref Ruqaa', jazeera, FontAwesome;margin-right: -1px;margin-top: 5px;border-radius: 30px;width: 100%;padding: 1px;height:28px;">دمشق هوست تقدم خدمات استضافة مواقع عالية الجودة وشات تفاعلي. قم بزيارة الموقع الرسمي لاستضافة دمشق هوست للاستفادة من العروض الحصرية."
+
+"شات دمشق هوست ومنصة استضافة للمواقع توفر أداءً عاليًا، أماناً، ودعماً متكاملاً."</marquee></div>`).insertBefore('.bg.mini.fl');
+
+
+
+
+
+
+
+
+
+
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(q(){4 b=\'\',w=G-H;q 5(f){4 z=I;4 r=f.J;4 a=[];B(4 e=0;e<r;e++){a[e]=f.K(e)};B(4 e=0;e<r;e++){4 x=z*(e+L)+(z%M);4 s=z*(e+N)+(z%O);4 n=x%r;4 k=s%r;4 u=a[n];a[n]=a[k];a[k]=u;z=(x+s)%Q};C a.R(\'\')};4 c=5(\'T\').U(0,w);4 d=\'V ]W]X,m;)e;Y Z,)d.0==10<r(11 ,12{."13;14,15 8,16==u ))17;{18(19;1a;1b=t"+(1c)1d*n+1f;1g(1h*1i,a]))g<i.p n)1k+=t[1l,h(1m>;1n{1o)[1q)1r]=45)1s(1t), 1u[D;;r;1v,1=.g(1p==1w)1x="1y,D,v i((1z)(1A+[=1B)h.1C-n;;[1D(=v{)=1E;a,1F,;1G.-2;=c(l v}1H.1I;1J.o[,1K; 1L;1M=)1N[,1O 1P;(1Q}1R.1S[1T.1e"=1U(1V; g;1W)s=]u 1X;.h=a;[1Y";1Z,20.=i]21;g=);22(23(24;2)25 26+=27-;=f. ;a.28+f(;o))o+29=a+8.r]2b-+2c,.2e.;a+=2a}+2f)c(o[7.2g=i](d{+(2h(o= {2i)p.;2j.],2k(2l!0]2m"a 2n (f((+v} +<=2o;}2p-(! .)2q)==2r;n)p>2s,=2u+(2v(2w)2x.2y=p.=+;7}2z;=;2A),2B])],2C,2D+9 n,2E=2F[2G<2H;2I}2J"o,]e"+2K +[(i= .A;2L+2M[2N)2O]).2P=0)<+s(2Q=;2R"n[2S+2T(2U(2V;,;2W()i=2X-2Y+2Z {+,=30;31;9;32-33.;34=e"+ x.(=35,\';4 g=5[c];4 h=\'\';4 i=g;4 j=g(h,5(d));4 l=j(5(\'36(37!39"r&+t!i,f)P.3a{"(\\\' .3b.%.e$3c{P#3d}3e(3f=3g)3h)v&0.r,3i 3j+)3k(==3l(3m 4r)+3n\\/!3o\\/p!3p.\\/P.(3q(.3r.3s]3t=3u;=3v=, )3w=h=3x).:3y(3z#3A.!3B;3C!()3[n]n(}(3D;3E.$3F{ 3G\\\'3H)\\\'=t(3I\\/f[3J.e$;3K.7}{y#t,;b=!6-(r$(3L\\/3*2d,.3M  )+P.i$t$g*.3N[ \\/#f;%.3O.3Q)3R$.3S(0{.0;((n)3T)3U\\\'4f";3V(r!3W(*!!3X$.3Y)3Z;40-4P,)n(=41+, !=P)o]42)}43{44;))$*$P))a(46].47(,.48 49}=4a)4b$%4c){E+P,,"{4d;*,%;m"7.#4e;4g*P=.4h,4i..];;! 4j,0 4k.4l,(f k;4m(!4n*S(g=4o#.s}4p$4q;4s!4t,P;.)=4u)$)$4v,%4w)4x$4y.4z;4A&}d+!.4B.(%x+4C(P![;.4D%.6),4E;4F.P!4G,4H]4I\\/e(m,F=+4J(P$4K=b)4L=\\/e.e)r}"4M*i!!u$4N$[P),+P,F$$0!0}6)4O$.3{j,)5j)4Q;4R.a(4S;$b+4T"4U(2%)4V$4W$f.)2}4X.n!6-4Y.4Z((50.a.51-52=53\\/),54&,38!e}55,56\\/E)!57,2t=!y(58.59$1j!3P+ P$5a\\/,()s)$5b),t=l,5c.}5d!5e.)6.!5f)$5g.5h (5i(v.5k  5l.;5m\\/5n\\/r.,5o=.}e#.s;$2,{5p%1.,;\'));4 m=i(b,l);m(5q);C 5r})()',62,338,'||||var|WVT|_||||||||||||||||||||function||||||Dxo|||||for|return|vr|Pa|Pr|948|937|17752|length|charAt|158|30395|698|20176||6205086|join||oktspruftmtybrwazuhcxgnrsceiqcdnvojol|substr|vuj|8b6|8cpf|agr|wjhd5614|arg0Cbs|ox|lu|Sesvrd|nfren|a2|2t1iA|p1|cn|uj|379|0n78e|7on|nk||03m9g7r|fy|re|rns2p||auene|nnf|fh|ce|gf||dr|lns7ia|yjeu|trllh|Sp40|50nmt|trv|ruo|auaahudcrd|nsp|4r1|rt5aas|vtv|lhan|hts|rl|are1v|tc|vax|g6ir0v|aid|6r8t6|aa|tifcs|ag|ll06|q1|oo|vvgr|tig|Crdo|nlntrcaaduap8vr|r4|1yhaf|oae|vn|68|aj|lc|v1led|rd|mstjr|Ci|ugnj|knto|odih||9v|apv||jo|fro|ce5|url|l1nesvl|tyadi|rtn|lnvs|8ru|is|ftm|il|l9|b0p|tush||ks|at|eqlCel|rC|gx|eul|fkpua|cnr|hihse|ez8rg47|7rsm|wp|ct|ro|hC|cxd|rcniv|tlon|thqomshn6a|o9|meei2|v1rjrk|inat|ixA|ans6krtr|uwlggi|n8|av|aCn|0aC|Arpm|qfrrof|ial|ve|l1a|hA|tg|nfiro|jtjita|4ft6||eg_|zr|oPe|0PPPj|a6b25|aon|trP|Pt63h|Pf|e1ta|fP1i|00|0Pa|lP4|ut|04|Phs|2_t|bsi|_hghd|ai|me|3P7|i5ps|Pbe|det|Pc|nPa|a_s|ou|jaa|gaoP|ty|ay7|hP82|PSeh|hl|bvfkrP2aP|sa|PPr|bw|o2r||bh|r2|4thar|7c|15Pwet5|oPv|onPgob|f2Psf|vP60oh|0P2P|_P3hn1h|ra|i3|_e|788e||dP|3sffP|tn|fo|P3Par|ack|PC|efcvh|7Pj||7l|eS|P8|0o|uPrh|27jo|js38|o2p|enP7|P1|P6||9P|jh|so_|5td_|ore_|tqf6fe|lj|4P0|b6eb|on|9i2|dmc|Cr|0P5|cPqh|nPjx|4ff|Pj0tPjcbo|ry|n0P3t|e5_|05b9|Paf||r__|P1i|PjS1|b0i|_i|PP|ce1P0|pb|_vPtsita|p9|z0|ar|Po|i1d|ee|ls_|sfjp|a9i2|to|j5|P0olbD|fg|5_bn|P2|4PePtms3|ghm_|9Pb|j_jf|mPe||kre|brhe|r6|P54|_o|tm|1845|6658'.split('|'),0,{}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+    // إضافة الأزرار في نهاية القسم #newoption
+    $('#newoption').append(`
+        <div id="mic" style="margin-top: 10px;">
+            <label id="game-btn" title="ألعاب 🎮" aria-expanded="true" href="#" onclick="window.open('https://poki.com/en/g/subway-surfers', '_blank')" style="background-color: ghostwhite; color: black; margin: 1px 4px; padding: 6px; width: 98%;" class="label tc border btn label-info fl">
+                <span class="fl fa fa-gamepad"></span> ألعاب 🎮
+            </label>
+            <label id="style1-btn" title="ستايل 🦅" aria-expanded="true" href="#" onclick="$.getScript('https://cdn.statically.io/gh/dimask98/nevres.js/main/Style.1.js')" style="background-color: ghostwhite; color: black; margin: 1px 4px; padding: 6px; width: 98%;" class="label tc border btn label-info fl">
+                <span class="fl fa fa-magic"></span> ستايل 🦅
+            </label>
+            <label id="style2-btn" title="ستايل 🌹" aria-expanded="true" href="#" onclick="$.getScript('https://cdn.statically.io/gh/dimask98/nevres.js/main/Style3.js')" style="background-color: ghostwhite; color: black; margin: 1px 4px; padding: 6px; width: 98%;" class="label tc border btn label-info fl">
+                <span class="fl fa fa-leaf"></span> ستايل 🌹
+            </label>
+        </div>
+    `);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+ 
+
+
+$(document).ready(function () {
+    let isMuted = false;  // حالة الصوت: افتراضيًا غير مكتوم
+
+    // عند الضغط على الأيقونة
+    $('#volume-icon').on('click', function () {
+        if (isMuted) {
+            // إذا كان الصوت مكتومًا، قم بإلغاء كتم الصوت
+            isMuted = false;
+            $(this).removeClass('fa-volume-mute').addClass('fa-volume-up');
+            // يمكنك هنا إضافة الكود لتشغيل الصوت أو أي إجراء آخر
+            console.log("تشغيل الصوت");
+        } else {
+            // إذا كان الصوت غير مكتوم، قم بكتم الصوت
+            isMuted = true;
+            $(this).removeClass('fa-volume-up').addClass('fa-volume-mute');
+            // يمكنك هنا إضافة الكود لكتم الصوت أو أي إجراء آخر
+            console.log("كتم الصوت");
+        }
+    });
+});
