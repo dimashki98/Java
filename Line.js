0fba12393f1d06acc79d49c7893bad8bf79de1b8
dimashki98 <najdshaqra@gmail.com>
2025-01-13 22:53:54 +0300

    Update Line1.js

--- Line.js
+++ Line.js
@@ -1,29 +1,3 @@
-$(document).ready(function() {
-    var socket;
-    
-    function connectWebSocket() {
-        socket = new WebSocket("wss://r7shq.com"); // استبدل بعنوان WebSocket الخاص بك
-
-        socket.onopen = function() {
-            console.log("تم الاتصال بالسيرفر.");
-        };
-
-        socket.onclose = function() {
-            console.log("تم فقد الاتصال، إعادة المحاولة...");
-            setTimeout(connectWebSocket, 3000); // إعادة الاتصال بعد 3 ثوانٍ
-        };
-
-        socket.onerror = function(error) {
-            console.log("خطأ في الاتصال:", error);
-            socket.close(); // إغلاق الاتصال وإعادة المحاولة
-        };
-    }
-
-    connectWebSocket();
-
-    // تعطيل تأثير فقدان الإنترنت في المتصفح
-    $(window).on("offline", function(event) {
-        event.preventDefault();
-        console.log("تم تجاهل انقطاع الإنترنت.");
-    });
-});
+setInterval(function () {
+    $(".ovr").remove();
+}, 1000); // يتحقق كل ثانية ويحذف العنصر إن وجد
