7f66ead460bb3fda15b7adb26ecb9ffbaa43f624
dimashki98 <najdshaqra@gmail.com>
2025-02-10 10:47:37 +0300

    Update Ping15.js

--- Ping15.js
+++ Ping15.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     // إضافة مقياس البينغ مع إشارة الواي فاي ووصف الحالة
     let pingWithWiFi = $('<div id="ping-container" style="padding: 6px; width: 98%; margin-left: 3px; margin-top: 2px; margin-bottom: 4px; font-family: Aref Ruqaa, sans-serif;">' +
-        '<span id="ping" style="font-weight: bold; color: black;">160ms</span>' +
+        '<span id="ping" style="font-weight: bold; color: black;">ms</span>' +
         '<div id="ping-meter" style="width: 100%; height: 8px; background-color: lightgrey; border-radius: 5px; margin-top: 8px;">' +
         '<div id="ping-bar" style="height: 100%; width: 0%; background-color: green; border-radius: 5px;"></div>' +
         '</div>' +
@@ -29,17 +29,17 @@ $(document).ready(function() {
             $("#ping").css("color", "green"); // سريع
             $("#ping-bar").css("background-color", "green"); // تغيير شريط المقياس إلى الأخضر
             $("#wifi-icon").css("color", "green"); // تغيير إشارة الواي فاي إلى الأخضر
-            $("#ping-status").text("حالة الشبكة: ممتاز").css("color", "green"); // وصف الحالة
+            $("#ping-status").text("حالة الشبكة: ممتازة").css("color", "green"); // وصف الحالة
         } else if (pingValue >= 100 && pingValue <= 200) {
             $("#ping").css("color", "orange"); // متوسط
             $("#ping-bar").css("background-color", "orange"); // تغيير شريط المقياس إلى البرتقالي
             $("#wifi-icon").css("color", "orange"); // تغيير إشارة الواي فاي إلى البرتقالي
-            $("#ping-status").text("حالة الشبكة: متوسط").css("color", "orange"); // وصف الحالة
+            $("#ping-status").text("حالة الشبكة: متوسطة").css("color", "orange"); // وصف الحالة
         } else {
             $("#ping").css("color", "red"); // بطيء
             $("#ping-bar").css("background-color", "red"); // تغيير شريط المقياس إلى الأحمر
             $("#wifi-icon").css("color", "red"); // تغيير إشارة الواي فاي إلى الأحمر
-            $("#ping-status").text("حالة الشبكة: رديء").css("color", "red"); // وصف الحالة
+            $("#ping-status").text("حالة الشبكة: رديئة").css("color", "red"); // وصف الحالة
         }
     }, 5000); // تحديث كل 5 ثوانٍ
 });
