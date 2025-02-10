0f022e29a38cbb2517d15655d48d2d306231e053
dimashki98 <najdshaqra@gmail.com>
2025-02-10 09:56:00 +0300

    Update and rename Ping3.js to Ping4.js

--- Ping4.js
+++ Ping4.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     let pingButton = $('<button class="border btn mini btn-success hand fl" ' +
         'style="padding: 6px; width: 98%; margin-left: 3px; margin-top: 2px; margin-bottom: 4px; font-family: Aref Ruqaa, sans-serif;">' +
-        '<span class="fl fa fa-wifi" style="font-family: FontAwesome, sans-serif;"></span> ' +
-        'البينغ <span id="ping" style="font-weight: bold; color: green;">160ms</span>' +
+        '<span class="fl fa fa-wifi" style="font-family: FontAwesome, sans-serif; color: white;"></span> ' +
+        '<span id="ping" style="font-weight: bold; color: white;">160ms</span>' +
         '</button>');
 
     $("button:contains('حـفـظ')").before(pingButton);
