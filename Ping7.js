41fcc5d4a7ab41eb4e29ea465649b7ae0ff4b068
dimashki98 <najdshaqra@gmail.com>
2025-02-10 10:01:37 +0300

    Update and rename Ping6.js to Ping7.js

---
+++ Ping7.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function() {
+    // إنشاء الزر الجديد
+    let pingButton = $('<button class="border btn mini hand fl" ' +
+        'style="padding: 6px; width: 98%; margin-left: 3px; margin-top: 2px; margin-bottom: 4px; font-family: Aref Ruqaa, sans-serif; background-color: white; color: black;">' +
+        '<span class="fl fa fa-wifi" style="font-family: FontAwesome, sans-serif; color: black;"></span> ' +
+        '<span id="ping" style="font-weight: bold; color: black;">160ms</span>' +
+        '</button>');
+
+    // إضافة الزر فوق زر "حفظ"
+    $("button:contains('حـفـظ')").before(pingButton);
+
+    // تحديث قيمة البينغ بشكل تفاعلي كل 5 ثوانٍ
+    setInterval(function() {
+        var pingValue = Math.floor(Math.random() * 300) + 50; // توليد قيمة بينغ عشوائية
+        var pingText = pingValue + "ms";
+
+        // تغيير النص
+        $("#ping").text(pingText);
+
+        // تغيير اللون بناءً على قيمة البينغ
+        if (pingValue < 100) {
+            $("#ping").css("color", "green"); // سريع
+        } else if (pingValue >= 100 && pingValue <= 200) {
+            $("#ping").css("color", "orange"); // متوسط
+        } else {
+            $("#ping").css("color", "red"); // بطيء
+        }
+    }, 5000); // تحديث كل 5 ثوانٍ
+});
