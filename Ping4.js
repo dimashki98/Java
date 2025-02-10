50a995a92c673ca4af123a45e0ba3513d99fd911
dimashki98 <najdshaqra@gmail.com>
2025-02-10 09:57:34 +0300

    Update Ping4.js

--- Ping4.js
+++ Ping4.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    let pingButton = $('<button class="border btn mini btn-success hand fl" ' +
-        'style="padding: 6px; width: 98%; margin-left: 3px; margin-top: 2px; margin-bottom: 4px; font-family: Aref Ruqaa, sans-serif;">' +
-        '<span class="fl fa fa-wifi" style="font-family: FontAwesome, sans-serif; color: white;"></span> ' +
-        '<span id="ping" style="font-weight: bold; color: white;">160ms</span>' +
+    let pingButton = $('<button class="border btn mini hand fl" ' +
+        'style="padding: 6px; width: 98%; margin-left: 3px; margin-top: 2px; margin-bottom: 4px; font-family: Aref Ruqaa, sans-serif; background-color: white; color: black;">' +
+        '<span class="fl fa fa-wifi" style="font-family: FontAwesome, sans-serif; color: black;"></span> ' +
+        '<span id="ping" style="font-weight: bold; color: black;">160ms</span>' +
         '</button>');
 
     $("button:contains('حـفـظ')").before(pingButton);
