b412c2e2719d267ffdfa1198f00ef4eaf54b1b5e
dimashki98 <najdshaqra@gmail.com>
2025-02-19 19:43:48 +0300

    Create Welcome10.js

---
+++ Welcome10.js
@@ -0,0 +1,55 @@
+    $("div#tlogins button.btn.btn-primary").click(function(){
+var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+   $(`
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url();" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="sico/"1730260909682.gif">
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">‎. 👑 وُردُهِہ‏‏ 💜  </span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك 👋 <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله </div>
+      </div>
+    </div></div>
+`).appendTo('div#d2')
+clearInterval(myVar);
+
+}else{console.log(usmsgw)} }, 2000);
+})
+
+$(`<img src="https://www.lldrh.com/sico/1730260909682.gif" class="fr" style="width:100%;margin-top: 1px;">
+<a href="" style="border-radius: 3px 12px 11px 11px;width:110px;margin:3px;" class="fl mini  fa fa-star-half-o btn btn-info">?الإشتراكات?</a>
+<a href="" style="border-radius: 3px 12px 11px 11px;width:110px;margin:3px;" class="fl mini fa fa-bell btn btn-danger">?القوانيـن?</a>  
+<a href="" style="border-radius: 3px 12px 11px 11px;width:110px;margin:3px;" class="fl mini  fa fa-heartbeat btn btn-info">?التطبيـق?</a>
+`).insertBefore('.nav-tabs');
+$("[data-target=#wall]").attr('title',' الحائط للأبداع  ')
+$('<img src="" style="width:85px;margin-top:-65px; " class="fr">').insertBefore('.loginstat');
+$("<br>").insertBefore('.checkbox');
+$('.nav-tabs').addClass('fl').css('width','100%');
+if(location.protocol!='https:'){location.protocol='https:';}
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="https://rawgit.com/muaml1/ahs0/master/%D9%81%D9%84%D8%A9%20%D8%A7%D9%84%D8%AE%D9%84%D9%8A%D8%AC.css">
+</style>`).insertBefore('body');
+$(".fl.ustat").css("width", "12px") 
+$(".fl.ustat").css("height", "12px") 
+$(".fl.ustat").css("border-radius", "12px") 
+$(".fl.ustat").css("position", "static") 
+$(".fl.ustat").css("margin-top", "10px") 
+$(".fl.ustat").css("margin-left", "0px")
+
+$(`<center><div><div width="99.5%" style="background-image:url(https://#);color: #000000;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;" ><font style="background-color: #35BDB4;border-radius: 1px 9px 9px 9px;width:87px;margin:2px;font-family: 'Helvetica',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10"<";    >اهلاً وسهلاً بكم في شات الوردة الملكيةة دمتم ب خير </div></div><center>`).insertBefore('#d2');
+
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" direction="right" style="margin-bottom: -9px;font-family: 'Changa', sans-serif;    height: 20px;color: #FFFFFF;"  onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">حياكم الله في شات الوردة الملكيةة نتشرف بتواجدكم .معنا. نقدم لكم ( مسابقات 🥇 ) - ( العاب ⚽) - ( ترفيه 🥇 ) ونتمنى لكم قضاء اجمل الاوقات معنا✨. مع تحيات الإدارة.😗😘 (  )
+</marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success')
+
+$(".btn .btn-primary" ).css( "border-radius", " 0px 25px 0px 25px");
+(document.getElementById("tbox").placeholder = "🌺🌺مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ")
+
+$(`<div style=" padding: 3px;margin: 2px; border: 1px solid #D6E3E3;height: 29px;width: 106px;" class="btn " type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">الادارة</div>
+<div class="collapse" id="collapseExample"><div style="text-align: center;font-family: 'jazeera';color: #702A2B;" class="well">
+<p class="bg-primary">..ℳσяαɒ.sy'🇸🇾.</p> ‎ ‎ ‎‎ ' .. اڸــۏڕدہ ̨اڷــمــڸــڬــۑْــہ♡& </div> 
+</div>`).insertBefore('a.fl.fa.fa-comment.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.upm.borderg');
