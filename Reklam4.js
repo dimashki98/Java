d45af0c92b6fd4efd0ab66b288fdef660a05aae8
dimashki98 <najdshaqra@gmail.com>
2025-03-07 03:46:22 +0300

    Update and rename Reklam3.js to Reklam4.js

--- Reklam4.js
+++ Reklam4.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
                             "padding": "10px",
                             "margin": "8px 0",
                             "background": "url('https://up6.cc/2025/03/174130790229041.gif') no-repeat center center", // الخلفية الجديدة (GIF)
-                            "background-size": "contain", // عرض الصورة بالكامل
+                            "background-size": "100% 100%", // تملأ المساحة بالكامل
                             "background-repeat": "no-repeat", // عدم تكرار الصورة
                             "box-shadow": "0 4px 8px rgba(0, 0, 0, 0.2)",
                             "transition": "all 0.3s"
@@ -52,7 +52,7 @@ $(document).ready(function () {
             "padding": "10px",
             "margin": "8px 0",
             "background": "url('https://up6.cc/2025/03/174130790229041.gif') no-repeat center center", // الخلفية الجديدة (GIF)
-            "background-size": "contain", // عرض الصورة بالكامل
+            "background-size": "100% 100%", // تملأ المساحة بالكامل
             "background-repeat": "no-repeat", // عدم تكرار الصورة
             "box-shadow": "0 4px 8px rgba(0, 0, 0, 0.2)",
             "transition": "all 0.3s"
