6582f191eade8cc5abee005065f68419ad215a4b
dimashki98 <najdshaqra@gmail.com>
2025-03-13 04:33:33 +0300

    Update and rename Mdx13.js to Mdx14.js

--- Mdx14.js
+++ Mdx14.js
@@ -1,5 +1,4 @@
-
-  $(document).on("setArrival", function() {
+setInterval(() => {
     $("#users .uzr").each(function() {
       let user = $(this);
       let decoText = user.find(".u-topic").text().trim(); // اجلب النص الفعلي
@@ -8,17 +7,18 @@
         user.find(".fitimg.u-pic").append("<img class='itarr_Mdx' title='اطاري' src='https://up6.cc/2023/03/16780297543541.png'>");
       }
     });
-  });
+  }, 3000);
 </script>
 
 <style>
+  /* CSS لضبط صورة الإطار */
   .itarr_Mdx {
     position: absolute;
-    bottom: 5px; /* تغيير الموقع إلى أسفل العنصر */
+    bottom: 0; /* تحديد الصورة أسفل العنصر */
     left: 50%;
     transform: translateX(-50%); /* لضبط الصورة في المنتصف */
-    width: 40px; /* ضبط العرض ليناسب نفس حجم hhq1 */
-    height: 40px; /* ضبط الارتفاع ليناسب نفس حجم hhq1 */
+    width: 30px; /* ضبط العرض ليناسب نفس حجم hhq1 */
+    height: 30px; /* ضبط الارتفاع ليناسب نفس حجم hhq1 */
     border-radius: 50%; /* جعل الصورة دائرية */
     z-index: 10; /* التأكد من أن الصورة تكون فوق العناصر الأخرى */
   }
