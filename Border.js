bed228e46224167e3195aa1c6759c477c961eceb
dimashki98 <najdshaqra@gmail.com>
2025-03-21 23:09:37 +0300

    Create Border.js

---
+++ Border.js
@@ -0,0 +1,7 @@
+setInterval(function() {
+  $('.ismute').css('border', '3px solid blue').fadeOut(200, function() {
+    $(this).css('border', '3px solid red').fadeIn(200, function() {
+      $(this).css('border', '3px solid yellow');
+    });
+  });
+}, 600);
