5f4c37ab5f8a331af44bdb84548cd7dd8f7c7ca7
dimashki98 <najdshaqra@gmail.com>
2025-03-21 23:41:53 +0300

    Update and rename Border1111111.js to Border11111111.js

--- Border1111111.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  setInterval(function() {
-    $('.fitimg.prod').css('border', '3px solid red');
-  }, 600);
-});
---
+++ Border11111111.js
@@ -0,0 +1,3 @@
+setInterval(function() {
+  $('.fitimg.prod').css('border', '3px solid red');
+}, 1000);  // يكرر تغيير اللون إلى الأحمر كل 1000 ميلي ثانية (1 ثانية)
