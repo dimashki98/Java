1c1bd27113987112904e2a3a5652e5893d6169b2
dimashki98 <najdshaqra@gmail.com>
2025-03-21 18:29:04 +0300

    Update Google.js

--- Google.js
+++ Google.js
@@ -2,61 +2,43 @@
 <div style="display:inline-block;text-align:center;margin-right:5px">
   <label class="label label-primary fa fa-image">ايقونه الموقع</label>
   <br>
-  <img class="p-logo" onclick="if (!window.__cfRLUnblockHandlers) return false; sendfilea(this,'logo');" style="width: 50px;height:50px;border: 2px solid darkgray;margin-top: 3px;" src="/site/<%= host %>logo.png" data-cf-modified-dea3edc0fd392f859d5b0256-="">
+  <img class="p-logo" onclick="if (!window.__cfRLUnblockHandlers) return false; sendfilea(this,'logo');" style="width: 50px;height:50px;border: 2px solid darkgray;margin-top: 3px;" src="/site/<%= host %>logo.png">
 </div>
-
-<script type="dea3edc0fd392f859d5b0256-text/javascript">
+<script>
   $(".p-logo").attr("src","/site/"+location.host+"logo.png")
 </script>
 
 <div style="display:inline-block;text-align:center;margin-right:5px">
   <label class="label label-primary fa fa-image">ايقونه الرسائل</label>
   <br>
-  <img class="p-msgpic" onclick="if (!window.__cfRLUnblockHandlers) return false; sendfilea(this,'msgpic');" style="width: 50px;height:50px;border: 2px solid darkgray;margin-top: 3px;" src="/site/msgpic.png" data-cf-modified-dea3edc0fd392f859d5b0256-="">
+  <img class="p-msgpic" onclick="if (!window.__cfRLUnblockHandlers) return false; sendfilea(this,'msgpic');" style="width: 50px;height:50px;border: 2px solid darkgray;margin-top: 3px;" src="/site/msgpic.png">
 </div>
-
-<script type="dea3edc0fd392f859d5b0256-text/javascript">
-  $(".p-msgpic").attr("src","/site/"+location.host+"msgpic.png")
-</script>
 
 <div style="display:inline-block;text-align:center;margin-right:5px">
   <label class="label label-primary fa fa-image">ايقونه الغرف</label>
   <br>
-  <img class="p-room" onclick="if (!window.__cfRLUnblockHandlers) return false; sendfilea(this,'room');" style="width: 50px;height:50px;border: 2px solid darkgray;margin-top: 3px;" src="/site/<%= host %>room.png" data-cf-modified-dea3edc0fd392f859d5b0256-="">
+  <img class="p-room" onclick="if (!window.__cfRLUnblockHandlers) return false; sendfilea(this,'room');" style="width: 50px;height:50px;border: 2px solid darkgray;margin-top: 3px;" src="/site/<%= host %>room.png">
 </div>
-
-<script type="dea3edc0fd392f859d5b0256-text/javascript">
+<script>
   $(".p-room").attr("src","/site/"+location.host+"room.png")
 </script>
 
 <div style="display:inline-block;text-align:center;margin-right:5px">
   <label class="label label-primary fa fa-image">ايقونه الاعضاء</label>
   <br>
-  <img class="p-user" onclick="if (!window.__cfRLUnblockHandlers) return false; sendfilea(this,'user');" style="width: 50px;height:50px;border: 2px solid darkgray;margin-top: 3px;" src="/site/<%= host %>pic.png" data-cf-modified-dea3edc0fd392f859d5b0256-="">
+  <img class="p-user" onclick="if (!window.__cfRLUnblockHandlers) return false; sendfilea(this,'user');" style="width: 50px;height:50px;border: 2px solid darkgray;margin-top: 3px;" src="/site/<%= host %>pic.png">
 </div>
-
-<script type="dea3edc0fd392f859d5b0256-text/javascript">
+<script>
   $(".p-user").attr("src","/site/"+location.host+"pic.png")
 </script>
 
 <div style="display:inline-block;text-align:center;margin-right:5px">
   <label class="label label-primary fa fa-image">ايقونه المايك</label>
   <br>
-  <img class="p-mic" onclick="if (!window.__cfRLUnblockHandlers) return false; sendfilea(this,'mic');" style="width: 50px;height:50px;border: 2px solid darkgray;margin-top: 3px;" src="/site/<%= host %>mic.png" data-cf-modified-dea3edc0fd392f859d5b0256-="">
+  <img class="p-mic" onclick="if (!window.__cfRLUnblockHandlers) return false; sendfilea(this,'mic');" style="width: 50px;height:50px;border: 2px solid darkgray;margin-top: 3px;" src="/site/<%= host %>mic.png">
 </div>
-
-<script type="dea3edc0fd392f859d5b0256-text/javascript">
+<script>
   $(".p-mic").attr("src","/site/"+location.host+"mic.png")
-</script>
-
-<div style="display:inline-block;text-align:center;margin-right:5px">
-  <label class="label label-primary fa fa-image">خلفية المايك</label>
-  <br>
-  <img class="p-bacmic" onclick="if (!window.__cfRLUnblockHandlers) return false; sendfilea(this,'bacmic');" style="width: 50px;height:50px;border: 2px solid darkgray;margin-top: 3px;" src="/site/<%= host %>bacmic.png" data-cf-modified-dea3edc0fd392f859d5b0256-="">
-</div>
-
-<script type="dea3edc0fd392f859d5b0256-text/javascript">
-  $(".p-bacmic").attr("src","/site/"+location.host+"bacmic.png")
 </script>
 
 <br><br>
