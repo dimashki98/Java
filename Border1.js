b658a601896d0c41c2c4675992a5cdab2cb0d10a
dimashki98 <najdshaqra@gmail.com>
2025-03-21 23:11:27 +0300

    Update and rename Border.js to Border1.js

--- Border1.js
+++ Border1.js
@@ -1,5 +1,5 @@
 setInterval(function() {
-  $('.ismute').css('border', '3px solid blue').fadeOut(200, function() {
+  $('.fitimg.prod').css('border', '3px solid blue').fadeOut(200, function() {
     $(this).css('border', '3px solid red').fadeIn(200, function() {
       $(this).css('border', '3px solid yellow');
     });
