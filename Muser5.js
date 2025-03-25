efef5b29294e3473621f4cf911ca4ded20bd56de
dimashki98 <najdshaqra@gmail.com>
2025-03-25 07:28:23 +0300

    Update and rename Muser1.js to Muser5.js

--- Muser5.js
+++ Muser5.js
@@ -4,13 +4,15 @@ $(document).ready(function () {
         mutationsList.forEach(function (mutation) {
             if (mutation.type === 'childList') {
                 $(mutation.addedNodes).each(function () {
-                    // التحقق إذا كان العنصر يحتوي على كلمة "الدمشقي"
+                    // التحقق إذا كان العنصر يحتوي على كلمة "الدمشقي" في .u-topic
                     if ($(this).find('.u-topic').text().includes("الدمشقي")) {
+                        // إضافة التأثير إلى الهيكل بالكامل
                         $(this).css({
                             "opacity": "0", // الشفافية صفر
                             "transform": "translateY(-30px) translateX(30px)", // تبدأ من أعلى وأسفل ومن اليمين
                             "transition": "opacity 0.8s ease-out, transform 0.8s ease-out", // التأثير التدريجي
-                            "border-radius": "15px" // جعل الحواف دايرية
+                            "border-radius": "15px", // جعل الحواف دايرية
+                            "border": "2px solid rgb(0, 0, 0)" // إضافة حدود دايرية
                         });
 
                         // بعد 50 ميلي ثانية تبدأ التغييرات تدريجياً
