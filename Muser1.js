059380afcb5b854f46becead08086eb66db1adee
dimashki98 <najdshaqra@gmail.com>
2025-03-25 07:18:07 +0300

    Update and rename Muser.js to Muser1.js

--- Muser1.js
+++ Muser1.js
@@ -9,14 +9,16 @@ $(document).ready(function () {
                         $(this).css({
                             "opacity": "0", // الشفافية صفر
                             "transform": "translateY(-30px) translateX(30px)", // تبدأ من أعلى وأسفل ومن اليمين
-                            "transition": "opacity 0.8s ease-out, transform 0.8s ease-out" // التأثير التدريجي
+                            "transition": "opacity 0.8s ease-out, transform 0.8s ease-out", // التأثير التدريجي
+                            "border-radius": "15px" // جعل الحواف دايرية
                         });
 
                         // بعد 50 ميلي ثانية تبدأ التغييرات تدريجياً
                         setTimeout(() => {
                             $(this).css({
                                 "opacity": "1", // تظهر تدريجياً
-                                "transform": "translateY(0) translateX(0)" // تتحرك إلى مكانها الطبيعي
+                                "transform": "translateY(0) translateX(0)", // تتحرك إلى مكانها الطبيعي
+                                "border-radius": "15px" // التأثير الدايري مستمر
                             });
                         }, 50);
                     }
