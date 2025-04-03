8d0b63bbc3816e2fc7baae5474785b01989d4918
dimashki98 <najdshaqra@gmail.com>
2025-04-03 11:39:44 +0300

    Update and rename Litee.js to Liteee.js

---
+++ Liteee.js
@@ -0,0 +1,21 @@
+console.log("وضع لايت مفعل ✅");
+
+// تعطيل جميع السكربتات الموجودة في الصفحة
+$("script").each(function () {
+    let scriptSrc = $(this).attr("src");
+
+    // استثناء Lite.js حتى لا يحذف نفسه
+    if (scriptSrc && scriptSrc.includes("Lite.js")) return;
+
+    $(this).remove(); // إزالة السكربت من الصفحة
+});
+
+// تعطيل جميع الأحداث المرتبطة بالعناصر
+$(document).off(); // تعطيل جميع الأحداث على document
+$("*").off(); // تعطيل جميع الأحداث على جميع العناصر
+
+// تعطيل كل الأنماط المتحركة والانتقالات
+$("*").css({
+    "animation": "none",
+    "transition": "none"
+});
