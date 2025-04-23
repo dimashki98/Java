7ed213dea999f5b39a4e35f259150ebf8c93b8ad
dimashki98 <najdshaqra@gmail.com>
2025-04-23 12:29:27 +0300

    Create Lamah.js

---
+++ Lamah.js
@@ -0,0 +1,125 @@
+$(`<center><div width="99.5%" style="background-image:url(sico/.jpeg);color: #595B61;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;"><font style="background-color: #fff;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;">مرحبـاً بكـم جميعاً في شـات لمة سوريا</font></div><center/>`).insertBefore('#d2');
+
+
+$(` <center><div style=" width: 99%;height: auto!important; " id="sa" margin:1px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+‏	<font color="#fff">&nbsp; •( إضغط هنا القرآن الكريم)• &#9679; &nbsp;</font></div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>
+‏	<b><font color="#000000" size="5">اريحوا مسامعكم </font></b></p><audio controls loop="" play=""><source src="https://h.top4top.io/m_1595scph70.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary')
+
+$(`<style>
+.fl.mini.u-msg {
+    color: #550C63!important;
+    text!important: ;
+}
+</style>`).insertBefore('body');
+$(`<a href="https://www.google.com/amp/s/www.pinterest.com/amp/yah275439/%25D8%25B1%25D9%2585%25D8%25B2%25D9%258A%25D8%25A7%25D8%25AA-%25D8%25A8%25D9%2586%25D8%25A7%25D8%25AA-%25D9%2584%25D9%2584%25D8%25AA%25D8%25B5%25D9%2585%25D9%258A%25D9%2585/" target="_blank" style=" font-family: 'El Messiri', sans-serif;width:107px;color: #E8415D;text-decoration:none; margin-right: 5px;
+margin-left: 2px;text-align: center;float: right;border-radius: 5px;
+border-color: #F2F2F2;
+padding: 3px;" class="border" ><i class="fa fa-forumbee"></i> رمزيات</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+
+$('.nav-tabs').addClass('fl').css('width','121%'); $('.nav-tabs').addClass('fl').css('width','100%'); $('.nav-tabs').addClass('fl').css('width','100%');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+
+#as {font-family: 'jazeera-light',FontAwesome;margin-bottom: 2px; color: #A68F81; border-radius: 5px 5px 5px 5px;
+    text-shadow: 2px 2px 2px rgb(138, 138, 138) , 0px 2px 10px #A68F81 !important;
+border: 1px solid #A68F81;
+height: 33px;
+text-align: center; 
+}
+.well {font-family: 'jazeera',FontAwesome;font-size: 16px!important;padding: 15px!important;text-align: center!important;width: 101%;color: #A68F81;white-space: nowrap;border-radius: 14px 14px 14px 14px;}
+.border {shadow:1px 1px 1px 1px ridge rgb(137, 137, 137); box-shadow:1px 1px 1px 1px rgb(137, 137, 137) }
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);border-radius: 5px 5px 5px 5px;;}
+ul.nav.nav-tabs.fl li a {      color: #fff;  background-image: linear-gradient( 220deg ,#313030 0,#939191 10%,#020202 51%,#939191 89%,#181717 100%);
+font-family: 'jazeera',FontAwesome;font-size: 12px!important;padding: 5px!important;text-align: center!important;width: 100%;}
+.label-primary, .btn-primary {
+    background-color: #ffffff;
+    background-image: linear-gradient( 220deg ,#313030 0,#939191 10%,#020202 51%,#939191 89%,#181717 100%);
+}
+ul.nav.nav-tabs.fl li {border-bottom: 2px solid rgb(138, 137, 137);width: 33.3%;}
+textarea.fl.filw.corner.tbox {    box-shadow:inset 0 0px 0px rgb(137, 137, 137); 0 0 7px rgb(137, 137, 137);border-radius: 7px;padding-left: 5px;}
+label.label.tc.border.btn.fl {border-radius: 41px 21px 41px 21px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 1px solid #A68F81;font-family: 'jazeera',FontAwesome;border-radius: 4px 4px 4px 4px;}
+label.fl.nosel.label.pnhead {font-family: 'jazeera',FontAwesome;width: 81%;text-align: -webkit-center;background-color: #A68F81;color: white;border-radius:13px 13px 13px 13px;
+background-image: linear-gradient( 220deg ,#313030 0,#939191 10%,#020202 51%,#939191 89%,#181717 100%);
+
+}
+.fr.borderg a {color: #A68F81;font-size: 12px!important;}
+.fr.borderg {color: #A68F81;font-size: 13px!important;border: 1px solid #A68F81;font-family: 'jazeera',FontAwesome;border-radius: 4px 4px 4px 4px;margin-top: 1px;}
+label.label-primary {border-radius: px 31px 31px 31px;}
+button.btn.fr.btn-success.fa.fa-refresh {
+padding: 4px 5px;
+background-color:"url()"});
+background-image::"url()"});
+border: 1px solid transparent;
+margin-top: -3px;
+border-radius: 50px;
+as:51px;
+}
+if(location.protocol!='https:'){location.protocol='https:';}
+
+.border.corner, .border.corner {box-shadow: inset 0 0px 0px rgb(137, 137, 137), 0 0 4px rgb(137, 137, 137);border: 2px solid #000000;as:13px;}
+.label-primary, .btn-primary, .ae.fa.label.fa-commenting-o.label-warning, .ae.fa.chats.label.fa-comment.label-warning {border: 1px solid #A68F81;border-radius: 31px 31px 31px 31px;as:31px;}
+.checkbox {color: rgb(137, 137, 137);}
+.checkbox label, .radio label {
+color: #000000;
+}#alshanfara{ 
+color: #ffff;
+    font-family: 'jazeera',FontAwesome;
+    font-size: 12px!important;
+    border: 1px solid #A68F81;
+text-align: center!important;
+box-shadow: inset 0 3px 3px rgb(137 137 137), 0 0 5px rgb(137 137 137);
+    background: #fdfdfd;
+background-image: linear-gradient( 220deg ,#313030 0,#939191 10%,#020202 51%,#939191 89%,#181717 100%);
+} .fr.borderg {
+    color: #000000;
+    border: 1px solid #3c3c3c;}.fr.borderg a {
+    color: #5c5c5c;
+    font-size: 12px!important;
+}
+
+.light {
+    background: repeating-linear-gradient( 
+45deg
+ ,rgba(255,255,255,0.8),rgb(255,255,255) 100px,#ffffffb0 0px,#fff6f699 200px ),repeating-linear-gradient( 
+-45deg
+ ,rgba(255,255,255,0.5),rgba(255,255,255,0.93) 100px,#b3adadd1 0px,#ffffffe6 200px )!important;
+
+}
+</style>`).insertBefore('body');
+$('.u-msg.dots').removeClass('dots');
+$("div#tlogins button.btn.btn-primary").click(function(){var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+   $(`
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://dl3.life/sico/1666243712100.jpg);" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="https://dl3.life/sico/1665503336893.gif"    >
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">شات لمة سوريا الصوتي</span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك 👋<h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1>    نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله في شات  لمة سوريا
+ </div>
+      </div>
+    </div></div>
+    `).appendTo('div#d2')
+clearInterval(myVar);
+
+}else{console.log(usmsgw)} }, 2000);
+})
+$(".fa-gear").text("ـ").css("width","");
+
+$(`<img src="" class="fr" style="width:0%;margin-top: 0px;">
+<a id="alshanfara" href="" style="border-radius: 0px 0px 0px 0px;width:24%;margin:2px;" class="fl mini  fa fa-
+btn btn-default"> الصوتي</a>
+<a id="alshanfara" href="" style="border-radius: 0px 0px 0px 0px;width:23%;margin:2px;" class="fl mini  fa fa-
+btn btn-default"> سوريا</a>
+<a id="alshanfara" href="" style="border-radius: 0px 0px 0px 0px;width:24%;margin:2px;" class="fl mini  fa fa-
+btn btn-default">لمة</a>
+<a id="alshanfara" href="" style="border-radius: 0px 0px 0px 0px;width:24%;margin:2px;" class="fl mini  fa fa-
+btn btn-default"> شات</a>
+<a id="alshanfara" href="https://dl3.life/" style="border-radius: 0px 0px 0px 0px;width:98%;margin:2px;" class="fl mini  fa fa-
+btn btn-default"> حياكم الله في شاتنا شات    " لمة سوريا"  الصوتي</a>`).insertBefore('.nav-tabs');
