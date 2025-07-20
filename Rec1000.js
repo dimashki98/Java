b4a85d49d0c75e2ab2f7693f591fae2d12ea4bdf
dimashki98 <najdshaqra@gmail.com>
2025-07-21 00:04:31 +0300

    Update and rename Rec100.js to Rec1000.js

--- Rec1000.js
+++ Rec1000.js
@@ -1,22 +1,15 @@
 $(function () {
-  var tried = 0;
-
   setInterval(function () {
     if (typeof socket !== "undefined") {
-      console.log("🛑 قطع الاتصال");
       socket.disconnect();
 
-      // بعد 3 ثواني، إعادة الاتصال
       setTimeout(function () {
-        console.log("🔁 إعادة الاتصال");
         if (typeof socket.connect === "function") {
           socket.connect();
         } else if (typeof NEW_CONNECT === "function") {
           NEW_CONNECT();
         }
       }, 3000);
-    } else {
-      console.log("⚠️ socket غير موجود");
     }
-  }, 3000); // كل 30 ثانية
+  }, 30000);
 });
